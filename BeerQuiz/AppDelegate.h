eda0f5c1f3d1fdb0bbdcbea779c566f04ef931dc
Richard Simko <richard@refinedwiki.com>
2017-02-21 17:14:33 +0100

    Initial Commit

---
+++ BeerQuiz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeerQuiz
+//
+//  Created by Richard Simko on 2017-02-21.
+//  Copyright © 2017 Richard Simko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeerQuiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeerQuiz
+//
+//  Created by Richard Simko on 2017-02-21.
+//  Copyright © 2017 Richard Simko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
